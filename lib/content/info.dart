ee3efa406505ffa03b1c1a0aa6a552c4a6a687e5
Dennis Jacob <dennis.jacob128@gmail.com>
2025-03-02 22:06:22 +0100

    feat: dashboard

--- lib/content/info.dart
+++ lib/content/info.dart
@@ -9,4 +9,22 @@ class Info {
     '1.8 - 1.9': 'Standing/walking work (waiter)',
     '2.0 - 2.4': 'Strenuous work (agrarian, high performance sport)',
   };
+
+  static final Map<String, String> bmiMale = {
+    '< 18.5': 'Underweight',
+    '18.5 – 24.9': 'Healthy weight',
+    '25 – 29.9': 'Overweight',
+    '30 – 34.9': 'Grade 1 obesity',
+    '35 – 39.9': 'Grade 2 obesity',
+    '> 40': 'Grade 3 obesity',
+  };
+
+  static final Map<String, String> bmiFemale = {
+    '< 17.5': 'Underweight',
+    '17.5 – 24': 'Healthy weight',
+    '24 – 29': 'Overweight',
+    '29 – 34': 'Grade 1 obesity',
+    '34 – 36.5': 'Grade 2 obesity',
+    '> 39': 'Grade 3 obesity',
+  };
 }
